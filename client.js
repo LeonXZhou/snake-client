6bbe05b6b8c04826097e9dc4f9f77f7c3a50047d
LeonXZhou <leon.zhou@queensu.ca>
2021-11-03 22:31:02 +0000

    added write name on connect

--- client.js
+++ client.js
@@ -7,6 +7,10 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log('connected');
+    conn.write("Name: LXZ");
+  });
   conn.on('data', (data) => console.log(data));
   return conn;
 };
--- play.js
+++ play.js
@@ -4,5 +4,4 @@ const {connect} = require("./client")
 
 console.log("Connecting ...");
 const conn = connect();
-conn.on('connect', () => console.log('connected'));
 conn.on('data', (data) => console.log(data)); 
